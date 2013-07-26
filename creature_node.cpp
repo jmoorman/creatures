d3b847f0b1f88f97e23cc8dcbc3b86f2c75d6b2f
Jon Moorman <moorman.jon@gmail.com>
2013-07-26 17:16:17 -0400

    creature node is taking shape

--- creature_node.cpp
+++ creature_node.cpp
@@ -2,13 +2,37 @@
 
 CreatureNode::CreatureNode(float scale, float density, 
                            const std::shared_ptr<btCollisionShape> &shape)
-    : scale_(scale), density_(density), shape_(shape)
+    : scale_(scale), density_(density), shape_(shape), body_(NULL)
 {
+}
+
+CreatureNode::CreatureNode(float scale, float density, btVector3 &dimensions) 
+    : scale_(scale), density_(density), body_(NULL)
+{
+   shape_(new btBoxShape(dimensions));
 }
 
 CreatureNode::~CreatureNode()
 {
-
+   delete body_;
 }
 
+void CreatureNode::AddToWorld(btDynamicsWorld *world, btVector3 &pos)
+{
+   btTransform start_transform;
+   start_transform.setOrigin(pos);
+   btDefaultMotionState *motion_state = 
+      new btDefaultMotionState(start_transform);
+   
+   //TODO: calculate mass as density * volume using hierarchical scale 
+   btScalar mass(1.f);
+   btVector3 local_inertia(0, 0, 0);
+   shape_->calculateLocalInertia(mass, local_inertia);
 
+   btRigidBody::btRigidBodyConstructionInfo info(mass, 
+                                                 motion_state,
+                                                 shape.get(),
+                                                 local_inertia);
+   body_ = new btRigidBody(info);
+   world->addRigidBody(body_);
+}
--- creature_node.h
+++ creature_node.h
@@ -9,16 +9,27 @@
 class CreatureNode
 {
  public:
-  CreatureNode(float scale, float density, const std::shared_ptr<btCollisionShape> &shape);
+
+  /*Create a new creature with the given dimensions*/
+  CreatureNode(float scale, float density, btVector3 &dimensions);
+  
+  /*Create a new creature using the given collision shape*/
+  CreatureNode(float scale, float density, 
+               const std::shared_ptr<btCollisionShape> &shape);
+  
   ~CreatureNode();
   
+  /*Initialize the physics of the node and add it to the simulation*/
+  void AddToWorld(btDynamicsWorld &world, btVector3 &pos);
+
   float scale_; // [0.5, 2]
   float density_; // [0 - 1]
   
-  /* Needs to be reused descendants but deleted when this genetic line 
+  /* 
+   * Needs to be reused descendants but deleted when this genetic line 
    * ceases to exist.
    */
-  std::shared_ptr<btCollisionShape> shape_; //This should be reused by descendants
+  std::shared_ptr<btCollisionShape> shape_; 
   
   btRigidBody *body_;
 };
